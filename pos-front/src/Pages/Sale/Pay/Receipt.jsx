9a09521bbd674bf634d1ecd4c7703ab27f109d5d
Choe-Ji-Hwan <ghldtjd901@naver.com>
2021-12-01 01:20:00 +0900

    + create receipt

--- pos-front/src/Pages/Sale/Pay/Receipt.jsx
+++ pos-front/src/Pages/Sale/Pay/Receipt.jsx
@@ -2,6 +2,33 @@ import styled from 'styled-components';
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import EventTable from "../../../Components/Table/EventTable";
+import TableContainer from "@material-ui/core/TableContainer";
+import Paper from "@material-ui/core/Paper";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import {Container} from "@material-ui/core";
+
+// td column style
+const ColumnCell = styled.td`
+    background-color: white;
+    font-size: 20px;
+    text-align: center;
+`;
+
+// tr style
+const ReceiptRow = styled.tr`
+    background-color: #FFFFFF;
+    height : 4vh;
+    &:focus {
+        background-color : #FF0000;
+    }
+`;
+
+// table style
+const ReceiptTableStyle = styled.table`
+    min-width: 400px;
+    width: 100%;
+`;
 
 const ReceiptTemplate = styled.div`
     background-color:#474D4E;
@@ -38,7 +65,7 @@ const Content = styled.div`
 const TopContent = styled.div`
     height : 55%;
     width : 100%;
-    border-bottom : 1px solid black;
+    border-bottom : 1px solid white;
     align-items : center;
     justify-content : center;
     text-align: center;
@@ -74,6 +101,16 @@ const TextDiv = styled.h2`
 
 const Receipt = ({orderId, setClick}) => {
 
+    const [paymentInfo, setPaymentInfo] = useState([]);
+    const [commonInfo, setCommonInfo] = useState({});
+
+    const [display,setDisplay]=useState(0);
+
+    useEffect(async ()=>{
+        await getReceipt();
+
+    },[]);
+
     const reIssueClickHandler = () =>{
         /*
         기기와 연돌할 것.
@@ -81,50 +118,51 @@ const Receipt = ({orderId, setClick}) => {
         alert("번호와 함께 영수증이 재출력 되었습니다.");
     }
 
-    useEffect(async ()=>{
-        try {
-            getTakeOutTicket().then(res =>{
-                console.log("가져온 TakeoutTicket 값:", res);
-            })
-            getReceipt().then(res => {
-                console.log("가져온 Receipt 값: " ,res);
-            })
-        } catch (e) {
-            console.log(e.message);
-        }
-    },[]);
-
-    const getTakeOutTicket = async () => {
-            let managerId = window.localStorage.getItem('managerId');
-            console.log("managerId", managerId);
-            console.log("orderId", orderId);
-            const data = {
-                branchId: managerId,
-                orderId: orderId,
-            }
-            // 데이터를 받아오는 동안 시간 소모. await 대기
-            await axios.post('http://localhost:8080/order/getTakeOutTicketInfo',  JSON.stringify(data), {
-                headers : {
-                    "Content-Type" : `application/json`,
-                }
-            })
-    }
-
     const getReceipt = async () => {
         let managerId = window.localStorage.getItem('managerId');
         const data = {
             branchId: managerId,
             orderId: orderId,
-            paymentId: -1,
         }
-        await axios.post('http://localhost:8080/payment/getReceipt', JSON.stringify(data), {
-            headers : {
-                "Content-Type" : `application/json`,
-            }
-        })
+        console.log(managerId);
+        console.log(orderId);
+        try {
+            await axios.post('http://localhost:8080/payment/getReceipt', JSON.stringify(data), {
+                headers: {
+                    "Content-Type": `application/json`,
+                }
+            }).then(res => {
+                console.log("가져온 Receipt 값: ", res.data);
+                const result = res.data;
+                const storeInfo = {
+                    'StoreName': result[0].StoreName,
+                    'Manager': result[0].Manager,
+                    'PhoneNumber': result[0].StorePhoneNumber,
+                    'Employee': result[0].Employee
+                };
+
+                const menuInfo = [['메뉴', '수량', '가격']];
+                for (let i = 1 ; i < result.length; i++){
+                    menuInfo.push([result[i].MenuName, result[i].Quantity, result[i].Price]);
+                }
+                console.log(menuInfo);
+                setCommonInfo(storeInfo);
+                setPaymentInfo(menuInfo);
+            })
+        } catch (e) {
+            console.log(e.message);
+        }
     }
 
-    const [display,setDisplay]=useState(0);
+
+    const showRow = (cells, ele) => {
+        return (
+            Array(cells.length).fill(undefined, undefined, undefined).map((obj, j)=>
+                <ColumnCell key={cells+j}>
+                    {cells[j]}
+                </ColumnCell>)
+        )
+    }
 
     return (
         <>
@@ -138,7 +176,41 @@ const Receipt = ({orderId, setClick}) => {
                         <TextDiv style={{paddingTop:'5vh'}}>+ 영수증 미리보기</TextDiv>
                         <TopContent style={{overflow: 'auto'}}>
                             <div >
-                                <ReceiptTextArea value={"dummy"}/>
+                                <TableContainer component={Paper} margin='10px'>
+                                    <ReceiptTableStyle>
+                                        <TableHead>
+                                            <ReceiptRow>
+                                                <ColumnCell/><ColumnCell style={{fontSize: '25px'}}>주문 내역</ColumnCell><ColumnCell/>
+                                            </ReceiptRow>
+                                        </TableHead>
+                                        <TableBody >
+                                            {Array(paymentInfo.length).fill(undefined, undefined, undefined).map((tr,i)=>
+                                                <ReceiptRow key={i}>
+                                                    {showRow(paymentInfo[i], i)}
+                                                </ReceiptRow>)}
+                                            <ReceiptRow>
+                                                {Array(3).fill(undefined, undefined, undefined).map((td, i)=>
+                                                <ColumnCell><hr style={{color:'#999999', borderStyle: 'dotted'}}/></ColumnCell>)}
+                                            </ReceiptRow>
+                                            <ReceiptRow>
+                                                <ColumnCell>가게이름: </ColumnCell>
+                                                <ColumnCell>{commonInfo['StoreName']}</ColumnCell>
+                                            </ReceiptRow>
+                                            <ReceiptRow>
+                                                <ColumnCell>가게대표: </ColumnCell>
+                                                <ColumnCell>{commonInfo['Manager']}</ColumnCell>
+                                            </ReceiptRow>
+                                            <ReceiptRow>
+                                                <ColumnCell>전화번호: </ColumnCell>
+                                                <ColumnCell>{commonInfo['PhoneNumber']}</ColumnCell>
+                                            </ReceiptRow>
+                                            <ReceiptRow>
+                                                <ColumnCell>담당직원: </ColumnCell>
+                                                <ColumnCell>{commonInfo['Employee']}</ColumnCell>
+                                            </ReceiptRow>
+                                        </TableBody>
+                                    </ReceiptTableStyle>
+                                </TableContainer>
                             </div>
                         </TopContent>
                         <BottomContent>
